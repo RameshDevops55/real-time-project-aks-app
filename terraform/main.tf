dc9ca711fff44019de00c5b39e4b7b4721ed6389
manoharanugu <142342787+manoharanugu@users.noreply.github.com>
2024-03-06 16:36:53 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,18 @@
 data "azurerm_client_config" "current" {}
 
-module "resource_group" {    
-  source    = "../modules/resourcegroup"
-  rg_name   = var.rg_name
-  location  = var.location  
-  tags      = var.tags
+module "resource_group" {    #this is resource we are using 
+  source    = "../modules/resourcegroup"   #indicate take this source from that perticular location and giving that path 
+  rg_name   = var.rg_name         #take this rgname form variables 
+  location  = var.location     #take this location of rg form variables 
+  tags      = var.tags      #take this tags of rg form variables 
 }
 
-module "aks" {    
-  source    = "../modules/aks"
-  depends_on = [ module.resource_group ]
-  rg_name   = var.rg_name
-  location  = var.location  
+module "aks" {        #this is resource we are using 
+  source    = "../modules/aks"     #indicate take this source from that perticular location and giving that path 
+  rg_name   = var.rg_name         #take this rgname form variables 
+  depends_on = [ module.resource_group ]   #it will tells after creating rg it will creates 
+  rg_name   = var.rg_name   #take this rgname form variables
+  location  = var.location   #take this location of rg form variables 
 }
 
 
