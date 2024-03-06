e618b2b65ad3bd7220d956de88457d49da3ea6c3
manoharanugu <142342787+manoharanugu@users.noreply.github.com>
2024-03-06 16:05:06 +0000

    Update aks_outputs.tf

--- modules/aks/aks_outputs.tf
+++ modules/aks/aks_outputs.tf
@@ -1,6 +1,8 @@
-output "client_certificate" {
-  value     = azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate
-  sensitive = true
+output "client_certificate" { #retrieving the client certificate for accessing an Azure Kubernetes Service (AKS) cluster. 
+  value     = azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate  #it's fetching the client certificate from the azurerm_kubernetes_cluster resource's kube_config attribute. The kube_config attribute typically contains a set of Kubernetes configuration settings, including the client certificate required for authenticating with the AKS cluster.
+  sensitive = true    #sensitive information like credentials or certificates, which should be kept private.
+
+
 }
 
 output "kube_config" {
