0246eaeb0f45814cec2faf024a396bbb7f2f8227
manoharanugu <142342787+manoharanugu@users.noreply.github.com>
2024-03-06 16:09:54 +0000

    Update aks_vars.tf

--- modules/aks/aks_vars.tf
+++ modules/aks/aks_vars.tf
@@ -1,9 +1,9 @@
 variable "location" {
     description =  "(Required) Keyvault Location"
-    default = "uksouth"  
+    default = "uksouth"  #it describes location of the AKS resource
 }
 
 variable "rg_name" {
     description =  "Name of the Resource Group"
-    default = "aks_terraform001"  
+    default = "aks_terraform001"  #it describes custom name of the resource group
 }
