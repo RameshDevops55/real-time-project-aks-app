b122369f1d04baa46b9759e1757f7bb49e9af822
manoharanugu <142342787+manoharanugu@users.noreply.github.com>
2024-03-06 16:38:46 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,6 @@ module "resource_group" {    #this is resource we are using
 
 module "aks" {        #this is resource we are using 
   source    = "../modules/aks"     #indicate take this source from that perticular location and giving that path 
-  rg_name   = var.rg_name         #take this rgname form variables 
   depends_on = [ module.resource_group ]   #it will tells after creating rg it will creates 
   rg_name   = var.rg_name   #take this rgname form variables
   location  = var.location   #take this location of rg form variables 
