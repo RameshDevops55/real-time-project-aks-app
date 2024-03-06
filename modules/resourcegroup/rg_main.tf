467780c5f8bec4b41c613d1846b6e14f31d247da
manoharanugu <142342787+manoharanugu@users.noreply.github.com>
2024-03-06 16:14:00 +0000

    Update rg_main.tf

--- modules/resourcegroup/rg_main.tf
+++ modules/resourcegroup/rg_main.tf
@@ -1,5 +1,5 @@
-resource "azurerm_resource_group" "example" {
-  name      = var.rg_name
-  location  = var.location
-  tags      = var.tags
+resource "azurerm_resource_group" "example" {  #this is resource we are taking 
+  name      = var.rg_name           #this is name of rg name defining in variable
+  location  = var.location          #this is location of rg n defining location name in variable
+  tags      = var.tags              #dictionary of key-value pairs mentioned in variable
 }
